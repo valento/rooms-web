2a4e446c127c7ed6a86d8925271a49a05b447613
valento <valentin.mundrov@gmail.com>
2026-02-07 00:26:06 +0200

    add/layout - grid, containers

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
  yarn create vite
 # pnpm
  pnpm create vite
-``
+```
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='grid-container black'>
+        <div className='widget'>sdf</div>
+        <div className='widget lg:col-span-2'>w4t</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>dfgdf</div>
+        <div className='widget lg:col-span-2'>w4t</div>
+        <div className='widget'>dfgdf</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>dfgdf</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>dfgdf</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>w4t</div>
+        <div className='widget'>dfgdf</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
