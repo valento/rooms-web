dfd67cb9608c076b54cd131fd92b63452ebad0b2
valento <valentin.mundrov@gmail.com>
2026-02-06 17:06:38 +0200

    init: vite, react-ts, tailwindcss

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwind(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+})
